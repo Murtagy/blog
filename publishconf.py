d7aba0d5fbb9598d62577ad989d93b70c2153bf8
Maksim R <maksim@dohop.com>
2022-07-12 10:48:36 +0300

    want to see visits

--- publishconf.py
+++ publishconf.py
@@ -18,4 +18,14 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+GOOGLE_ANALYTICS = "322729420"
+
+# Take advantage of the following defaults
+# STATIC_SAVE_AS = '{path}'
+# STATIC_URL = '{path}'
+STATIC_PATHS = [
+    'static/robots.txt',
+    ]
+EXTRA_PATH_METADATA = {
+    'static/robots.txt': {'path': 'robots.txt'},
+    }
