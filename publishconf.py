f9d0f483aa407c2263f5239b2da1d31b7af80546
Maksim R <maksim@dohop.com>
2022-12-22 12:00:40 +0300

    favicon

--- publishconf.py
+++ publishconf.py
@@ -15,17 +15,17 @@ CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
 GOOGLE_ANALYTICS = "UA-234232113-1"
 
 # Take advantage of the following defaults
 # STATIC_SAVE_AS = '{path}'
 # STATIC_URL = '{path}'
-STATIC_PATHS = [
-    'static/robots.txt',
-    ]
+
+# A list of directories (relative to PATH (default to content dir)) in which to look for static files. Such files will be copied to the output directory without modification.
+STATIC_PATHS = ['static', 'images']
+
+# Copy static files to the root of the site
 EXTRA_PATH_METADATA = {
     'static/robots.txt': {'path': 'robots.txt'},
-    }
+    'static/favicon.ico': {'path': 'favicon.ico'},
+}
